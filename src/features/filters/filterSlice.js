e2524493200aaf13bcae9d2a2ce30104da421cba
kuon609 <kuon@prog-8.com>
2020-12-28 17:25:16 +0900

    add combineReducers

---
+++ src/features/filters/filterSlice.js
@@ -0,0 +1,42 @@
+const initialState = {
+  status: 'All',
+  colors: [],
+}
+
+export default function filtersReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'filters/statusFilterChanged': {
+      return {
+        ...state.filters,
+        status: action.payload,
+      }
+    }
+    case 'filters/colorFilterChanged': {
+      let { color, changeType } = action.payload
+      const { colors } = state
+
+      switch (changeType) {
+        case 'added': {
+          if (colors.includes(color)) {
+            return state
+          }
+
+          return {
+            ...state,
+            colors: colors.concat(color),
+          }
+        }
+        case 'removed': {
+          return {
+            ...state,
+            colors: colors.filter((existingColor) => existingColor !== color),
+          }
+        }
+        default:
+          return state
+      }
+    }
+    default:
+      return state
+  }
+}
--- src/reducer.js
+++ src/reducer.js
@@ -1,18 +1,10 @@
-const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
-  ],
-  filters: {
-    status: 'All',
-    colors: [],
-  },
-}
+import { combineReducers } from 'redux'
+import todosReducer from './features/todos/todoSlice'
+import filterReducer from './features/filters/filterSlice'
 
-export default function appReducer(state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state
-  }
-}
+const rootReducer = combineReducers({
+  todos: todosReducer,
+  filters: filterReducer,
+})
+
+export default rootReducer
