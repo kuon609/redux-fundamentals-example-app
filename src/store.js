43a872917cdcbf8e4ace463a2d8d5c8e5a800bed
kuon609 <kuon@prog-8.com>
2020-12-30 18:57:04 +0900

    add middleware

--- src/store.js
+++ src/store.js
@@ -1,15 +1,29 @@
-import { createStore } from 'redux'
+// import { createStore, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
+import { print1, print2, print3 } from './exampleAddons/middleware'
 
-let preloadedState
-const persistedTodosString = localStorage.getItem('todos')
+const composedEnhancer = composeWithDevTools(
+  applyMiddleware(print1, print2, print3)
+)
 
-if (persistedTodosString) {
-  preloadedState = {
-    todos: JSON.parse(persistedTodosString),
-  }
-}
+// import {
+//   includeMeaningOfLife,
+//   sayHiOnDispatch,
+// } from './exampleAddons/enhancers'
 
-const store = createStore(rootReducer, preloadedState)
+// let preloadedState
+// const persistedTodosString = localStorage.getItem('todos')
+
+// if (persistedTodosString) {
+//   preloadedState = {
+//     todos: JSON.parse(persistedTodosString),
+//   }
+// }
+
+// const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
+
+const store = createStore(rootReducer, composedEnhancer)
 
 export default store
