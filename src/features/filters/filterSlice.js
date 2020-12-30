a8892b192617aa6c35431f4c281078cc827fdd53
kuon609 <kuon@prog-8.com>
2020-12-30 17:59:16 +0900

    add store

--- src/features/filters/filterSlice.js
+++ src/features/filters/filterSlice.js
@@ -7,7 +7,7 @@ export default function filtersReducer(state = initialState, action) {
   switch (action.type) {
     case 'filters/statusFilterChanged': {
       return {
-        ...state.filters,
+        ...state,
         status: action.payload,
       }
     }
